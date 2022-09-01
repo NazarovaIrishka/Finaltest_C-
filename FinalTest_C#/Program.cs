9cf1a7d9764743ff18cb14da841c0ab1419e584d
Irina <marimashka96@mail.ru>
2022-09-01 18:23:41 +0300

    Create method CreateAndFillArray

--- FinalTest_C#/Program.cs
+++ FinalTest_C#/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] CreateAndFillArray ()
+{
+    Console.Write("Enter the number of array elements: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] Array1 = new string[size]; 
+    for (int i = 0; i < Array1.Length; i++)
+    {
+        Console.Write("Enter array element: ");
+        Array1 [i] = Console.ReadLine();
+    }
+    return Array1;
+}
